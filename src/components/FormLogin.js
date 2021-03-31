5d2393eeaaffeba3f3ada814ea245d0c76626004
alessandrovaru <alessandrovaru@gmail.com>
2021-03-30 23:32:15 -0400

    Cors anywhere

--- src/components/FormLogin.js
+++ src/components/FormLogin.js
@@ -17,7 +17,7 @@ const FormLogin = ({
 
     try {
       const response = await fetch(
-        "https://network-api.onefootball.com/v1/login/",
+        "https://cors-anywhere.herokuapp.com/https://network-api.onefootball.com/v1/login/",
         requestOptions
       );
       const data = await response.json();
